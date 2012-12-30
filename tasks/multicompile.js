6444edac563310a885288159e8e9bbbe8a202f55
Ian Beveridge <ian.beveridge@gmail.com>
2012-12-30 09:07:23 +0000

    Last release completely broke JS compile. Now 0.9

--- tasks/multicompile.js
+++ tasks/multicompile.js
@@ -1,27 +1,50 @@
 /*global require:true,console:true*/
 module.exports = function(grunt) {
 
-    'use strict';
-
     var path = require('path');
     var shouldBuild = require('./lib/shouldBuild');
 
+    function deepCopy(obj) {
+        var i;
+        var out;
+
+        if (Object.prototype.toString.call(obj) === '[object Array]') {
+            out = [];
+            var len = obj.length;
+            for (i=0 ; i < len; i++ ) {
+                out[i] = arguments.callee(obj[i]);
+            }
+            return out;
+        }
+        if (typeof obj === 'object') {
+            out = {};
+            for ( i in obj ) {
+                out[i] = arguments.callee(obj[i]);
+            }
+            return out;
+        }
+        return obj;
+    }
     /** Like closureCompiler, but minifies source files individually without concat (require.js has already concatted enough) */
     grunt.registerMultiTask('multiCompile', 'Call a grunt task on a group of source files one at a time individually', function() {
-        var files = grunt.file.expandFiles(this.data.src);
-        var opts = this.data.opts;
+
+        'use strict';
+
+        var data = this.data;
+        var files = grunt.file.expandFiles(data.src);
+        var opts = deepCopy(data.opts);
         var outdir = opts.output_file;
-        var leading = this.data.srcDir.length;
+        var leading = data.srcDir.length;
         var done = this.async();
 
         var compile = function(f, files, maps) {
             opts.js = f.replace(/\\/g, '/');
-            var output_file = path.join(outdir, f.substr(leading)).replace(/\\/g, '/');
-            output_file = grunt.template.process(output_file);
-            grunt.file.mkdir(path.dirname(output_file));
+            opts.output_file = path.join(outdir, f.substr(leading)).replace(/\\/g, '/');
+            opts.output_file = grunt.template.process(opts.output_file);
+            grunt.file.mkdir(path.dirname(opts.output_file));
             opts.checkModified = true;
 
-            if(!shouldBuild(grunt, output_file, [opts.js])) {
+            if(!shouldBuild(grunt, opts.output_file, [opts.js])) {
                 if(files.length===0) {
                     done();
                     return;
