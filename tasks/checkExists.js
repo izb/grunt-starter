9d289c91e9057856acc410877e312de89f2e4fde
Ian Beveridge <ian.beveridge@gmail.com>
2013-11-04 17:34:10 +0000

    Remove console output

--- tasks/checkExists.js
+++ tasks/checkExists.js
@@ -6,7 +6,6 @@ module.exports = function(grunt) {
     grunt.registerMultiTask( "checkExists", "Checks for the existence of given paths and fails the build if they're not there.", function() {
 
         this.data.paths.forEach(function(f) {
-            console.log("TEST", f, grunt.file.expand(f));
             if (grunt.file.expand(f).length===0) {
                 grunt.fail.fatal(this.data.err);
             }
